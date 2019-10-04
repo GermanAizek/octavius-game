144d35e365199540c580eb2b191f48ad42a490c1
GermanAizek <GermanAizek@yandex.ru>
2019-10-04 17:06:11 +0300

    [ADD] Added test code

---
+++ octavius.cpp
@@ -0,0 +1,131 @@
+﻿#include "octavius.h"
+
+void init()
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		cout << "Unable to init SDL, error: " << SDL_GetError() << endl;
+		exit(1);
+	}
+
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+	SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 5);
+	SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 6);
+	SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 5);
+
+	window = SDL_CreateWindow("Slot machine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+
+	SDL_GLContext glcontext = SDL_GL_CreateContext(window);
+
+	if (window == NULL)
+	{
+		exit(1);
+	}
+
+	// init opengl
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClearDepth(1.0);
+	glDepthFunc(GL_LESS);
+	glEnable(GL_DEPTH_TEST);
+	glShadeModel(GL_SMOOTH);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0f, (float)width / (float)height, 0.1f, 100.0f);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+void drawCube(float xrf, float yrf, float zrf) {
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	glLoadIdentity();
+	glTranslatef(0.0f, 0.0f, -7.0f);
+
+	glRotatef(xrf, 1.0f, 0.0f, 0.0f);
+	glRotatef(yrf, 0.0f, 1.0f, 0.0f);
+	glRotatef(zrf, 0.0f, 0.0f, 1.0f);
+
+	glBegin(GL_QUADS);
+
+	glColor3f(0.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+
+	glColor3f(1.0f, 0.5f, 0.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+
+	glColor3f(1.0f, 1.0f, 0.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, 1.0f);
+	glVertex3f(-1.0f, 1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, -1.0f);
+	glVertex3f(-1.0f, -1.0f, 1.0f);
+
+	glColor3f(1.0f, 0.0f, 1.0f);
+	glVertex3f(1.0f, 1.0f, -1.0f);
+	glVertex3f(1.0f, 1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, 1.0f);
+	glVertex3f(1.0f, -1.0f, -1.0f);
+
+	glEnd();
+}
+
+int main()
+{
+	init();
+
+	bool running = true;
+	float xrf = 0, yrf = 0, zrf = 0;
+	while (running)
+	{
+		SDL_Event event;
+		while (SDL_PollEvent(&event))
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				running = false;
+				break;
+
+			case SDL_KEYDOWN:
+				switch (event.key.keysym.sym)
+				{
+				case SDLK_ESCAPE:
+					running = false;
+					break;
+				}
+				break;
+			}
+		}
+
+		// calc rotate
+		xrf -= 0.5;
+		yrf -= 0.5;
+		zrf -= 0.5;
+
+		drawCube(xrf, yrf, zrf);
+
+		// refresh
+		glFlush();
+		SDL_GL_SwapWindow(window);
+	}
+
+	SDL_Quit();
+
+	return 0;
+}
---
+++ octavius.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "SDL.h"
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+#include <iostream>
+using namespace std;
+
+SDL_Window* window;
+
+const int width = 640;
+const int height = 480;
+
+void drawCube(float xrf, float yrf, float zrf);
